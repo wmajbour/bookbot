ae1683c0b3909d2ba754a8340e464d10a9e945e4
wmajbour <wmajbour@Hotmail.com>
2025-06-30 05:30:36 -0400

    C: added dict sorting

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from stats import get_num_words, count_each_char
 
+imported_book = "books/frankenstein.txt"
 
 def main():
+    print("============ BOOKBOT ============")
+    print(f"Analyizing book found at {imported_book}")
+    print("------------ Word Count ------------")
     get_num_words("books/frankenstein.txt")
+    print("------------ Character Count ------------")
     count_each_char("books/frankenstein.txt")
+    print("============ END ============")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def get_book_name(book):
+    return book
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
@@ -5,17 +8,22 @@ def get_book_text(path_to_file):
 def get_num_words(book):
     parsed = get_book_text(book)
     num_words = parsed.split()
-    print(f"{len(num_words)} words found in the document")
+    print(f"Found {len(num_words)} total words")
 
 def count_each_char(book):
     characters = get_book_text(book).lower()
     char_dict = {}
     for i in characters:
-        if i in char_dict:
-            char_dict[i] += 1
-        else:
-            char_dict[i] = 1
+        if i.isalpha():
+            if i in char_dict:
+                char_dict[i] += 1
+            else:
+                char_dict[i] = 1
+    char_items = list(char_dict.items())
+    char_items.sort(reverse=True, key=sort_dict)
+    
+    for char, count in char_items:
+        print(f"{char}: {count}")
 
-    print(f"{char_dict}\n") 
-
-            
+def sort_dict(items):
+    return items[1]
